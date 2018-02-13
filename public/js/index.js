34b8a7bb2c338a6efb2498bfd9e84609cf7e3cb0
Çağatay Sarp Aksu <sarpcaksu@gmail.com>
2018-02-13 16:45:21 +0300

    UserLoginController doesn't need to be copied

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,4 @@
 ConnectionController.initialize();
 window.onload = function _onBodyLoad(){
-    var ULC = Object.create(UserLoginController);
-    ULC.Show(document.body);
+    UserLoginController.Show(document.body);
 }
